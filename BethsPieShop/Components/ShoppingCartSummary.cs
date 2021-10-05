e2a1e913c2431c6534f4642b28dba69801d88d0e
Matthew Castrigno <matthewcastrigno@u.boisestate.edu>
2021-10-05 11:01:07 -0600

    chapter 7 complete and working

---
+++ BethsPieShop/Components/ShoppingCartSummary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using BethsPieShop.Models;
+using BethsPieShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BethsPieShop.Components
+{
+    public class ShoppingCartSummary: ViewComponent
+    {
+        private readonly ShoppingCart _shoppingCart;
+
+        public ShoppingCartSummary(ShoppingCart shoppingCart)
+        {
+            _shoppingCart = shoppingCart;
+        }
+        // this IViewComponetResult is a very specific type for components but still returns a view
+        public IViewComponentResult Invoke()
+        {
+
+            var items = _shoppingCart.GetShoppingCartItems();
+            _shoppingCart.ShoppingCartItems = items;
+
+            var shoppingCartViewModel = new ShoppingCartViewModel
+            {
+                ShoppingCart = _shoppingCart,
+                ShoppingCartTotal = _shoppingCart.GetShoppingCartTotal()
+
+            };
+            return View(shoppingCartViewModel);
+        }
+    }
+}
--- BethsPieShop/Controllers/PieController.cs
+++ BethsPieShop/Controllers/PieController.cs
@@ -20,21 +20,44 @@ namespace BethsPieShop.Controllers
             _pieRepository = pieRepository;
             _categoryRepository = categoryRepository;
         }
-         public ViewResult List()
+
+        public ViewResult List(string category)
         {
-            PiesListViewModel piesListViewModel = new PiesListViewModel();
-            piesListViewModel.Pies = _pieRepository.AllPies;
-            piesListViewModel.CurrentCategory = "Cheese Cakes";
-            return View(piesListViewModel);
+            IEnumerable<Pie> pies;
+            string currentCategory;
+
+            if (string.IsNullOrEmpty(category))
+            {
+                pies = _pieRepository.AllPies.OrderBy(p => p.PieId);
+                currentCategory = "All pies";
+            }
+            else
+            {
+                pies = _pieRepository.AllPies.Where(p => p.Category.CategoryName == category)
+                    .OrderBy(p => p.PieId);
+                currentCategory = _categoryRepository.AllCategories.FirstOrDefault(c => c.CategoryName == category)?.CategoryName;
+            }
+
+            return View(new PiesListViewModel
+            {
+                Pies = pies,
+                CurrentCategory = currentCategory
+            });
         }
+        // public ViewResult List()
+        //{
+        //    PiesListViewModel piesListViewModel = new PiesListViewModel();
+        //    piesListViewModel.Pies = _pieRepository.AllPies;
+        //    piesListViewModel.CurrentCategory = "Cheese Cakes";
+        //    return View(piesListViewModel);
+        //}
     
         public IActionResult Details(int id)     
 	{
             var pie = _pieRepository.GetPieById(id);
             if (pie == null)
-            
-                return NotFound();
-                return View(pie);
+                 return NotFound();
+            return View(pie);
            
 
 	}
